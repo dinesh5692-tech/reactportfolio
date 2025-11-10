2e542e2a56b6da09b3973e05f39711493a8f8b57
dinesh5692-tech <18n81a05n9manoj@gmail.com>
2025-11-10 14:09:45 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,11 +5,11 @@ const About = () => {
     <section id="about" className="py-20 px-10 bg-white">
       <h2 className="text-4xl font-bold mb-6 text-center">About Me</h2>
       <p className="max-w-3xl mx-auto text-center text-lg">
-        Hi! I’m Ragasree, a Software Engineer and Front-End Developer passionate about building clean, responsive, and user-friendly websites and applications. I love turning ideas into interactive digital products using React, JavaScript, Next.js, and Node.js.
+        Hi! I’m Manoj kumar, a Software Engineer and Front-End Developer passionate about building clean, responsive, and user-friendly websites and applications. I love turning ideas into interactive digital products using React, JavaScript, Next.js, and Node.js.
 
-With a professional background at Infosys, I worked on everything from optimizing infrastructure to enhancing user interfaces and resolving complex issues. Now, as a graduate student at UNT, I continue to sharpen my skills in web development, automation, and system design.
+With a professional background at ITSS Global, I worked on everything from optimizing infrastructure to enhancing user interfaces and resolving complex issues.When I’m not coding, I’m exploring design trends, learning new technologies, and looking for opportunities to create impactful digital experiences.
 
-When I’m not coding, I’m exploring design trends, learning new technologies, and looking for opportunities to create impactful digital experiences. I’m open to Frontend Developer roles where I can innovate, collaborate, and grow
+ I’m open to Frontend Developer roles where I can innovate, collaborate, and grow
       </p>
     </section>
   );
